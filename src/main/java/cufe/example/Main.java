6365a6730bef71dd7d9932a4271b88ebc4176f17
FJWangYantao <a1127323408@163.com>
2024-10-09 10:34:54 +0800

    Initial files

---
+++ src/main/java/cufe/example/Main.java
@@ -0,0 +1,10 @@
+package cufe.example;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
